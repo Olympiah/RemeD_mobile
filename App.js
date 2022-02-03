f5110f26e8437b5ba6312ba566c867588635ecdb
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-02-03 17:30:12 +0300

    Added a liitle touch to login

--- App.js
+++ App.js
@@ -7,17 +7,23 @@ import DoctorDash from "./screens/dashboard/DoctorDash";
 import PatientDash from "./screens/dashboard/PatientDash";
 import DoctorProfileScreen from "./screens/Profiles/DoctorProfileScreen";
 import PatientProfileScreen from "./screens/Profiles/PatientProfileScreen";
-import Login from "./screens/Auth/Login";
-import Register from "./screens/Auth/Register";
 import ContextProvider from "./context/state";
 import Notes from "./screens/Notes/Notes"
 import AddNote from "./screens/Notes/AddNote"
+import 'react-native-gesture-handler';
+
+import Landing from './screens/Landing';
+import Signin from './screens/Loginscreens/Signin';
+import Signup from './screens/Loginscreens/Signup';
+
 // import theme from './theme'
 // import { theme } from "./utils/Theme";
 import useFonts from "./hooks/useFonts"
 import AppLoading from "expo-app-loading"
 
 const Stack = createNativeStackNavigator();
+const Tab= createNativeStackNavigator();
+
 
 const App = () => {
 
@@ -37,12 +43,30 @@ const App = () => {
     )
   }
 
+
+
+  function Land() {
+    return(
+       <Tab.Navigator>
+           <Tab.Screen name='Landing' component={Landing}/>
+           <Tab.Screen name='Signin' component={Signin}/>
+           <Tab.Screen name='Signup' component={Signup}/>
+       </Tab.Navigator>
+    );
+ }
+
   return (
     <NativeBaseProvider>
       <ContextProvider>
         <NavigationContainer>
           <Stack.Navigator>
+            <Stack.Screen
+              name='Land'
+              component={Land}
+              options={{headerShown:false}
+           }/> 
             <Stack.Screen name="Home" component={HomeScreen} />
+
             <Stack.Screen name="DoctorProfile" component={DoctorProfileScreen} />
             <Stack.Screen
               name="PatientProfile"
@@ -50,8 +74,6 @@ const App = () => {
             />
             <Stack.Screen name="DoctorDash" component={DoctorDash} />
             <Stack.Screen name="PatientDash" component={PatientDash} />
-            <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
-            <Stack.Screen options={{ headerShown: false }} name="Register" component={Register} />
             <Stack.Screen options={{ headerShown: false }} name="Notes" component={Notes} />
             <Stack.Screen options={{ headerShown: false }} name="AddNote" component={AddNote} />
           </Stack.Navigator>
---
+++ screens/Loginscreens/Signup.js
@@ -0,0 +1,325 @@
+import { Platform, StyleSheet, StatusBar,Text, View,TouchableOpacity,Dimensions } from 'react-native';
+import React from 'react';
+
+import { LinearGradient } from 'expo-linear-gradient';
+import Feather from 'react-native-vector-icons/Feather';
+import { Input, Icon } from 'react-native-elements';
+import  * as Animatable from 'react-native-animatable';
+
+const Signup = ({navigation}) => {
+    const[data,setData]= React.useState({
+        email:'',
+        password:'',
+        name:'',
+        confirm_password:'',
+        check_textInputChange:false,
+        check_nameInputChange:false,
+        secureTextEntry:true,
+        confirm_secureTextEntry:false
+     });
+   
+     const textInputChange =(val)=>{
+       if(val.length != 0) {
+         setData({
+           ...data,
+           email:val,
+           check_textInputChange:true
+         });
+       } 
+       else{
+         setData({
+           ...data,
+           email:val,
+           check_textInputChange:false
+         });  
+       }
+     }
+  
+     const nameInputChange =(val)=>{
+      if(val.length != 0) {
+        setData({
+          ...data,
+          name:val,
+          check_nameInputChange:true
+        });
+      } 
+      else{
+        setData({
+          ...data,
+          name:val,
+          check_nameInputChange:false
+        });  
+      }
+    }
+   
+     const handlePasswordChange=(val)=>{
+       setData({
+         ...data,
+         password:val
+       });
+     }
+     const handleConfirmPasswordChange=(val)=>{
+      setData({
+        ...data,
+        confirm_password:val
+      });
+    }
+   
+     const updateSecureTextEntry=()=>{
+       setData({
+         ...data,
+         secureTextEntry: !data.secureTextEntry
+       })
+     }
+  
+     const updateConfirmSecureTextEntry=()=>{
+      setData({
+        ...data,
+        confirm_secureTextEntry: !data.confirm_secureTextEntry
+      })
+    }
+   
+     return (
+       <View style={styles.container}>
+         <StatusBar backgroundColor={'white'} barStyle='light-content'/>
+         <View style={styles.header}>
+           <Text style={styles.text_header}>Create account Now!</Text>
+         </View>
+         <Animatable.View 
+           animation='fadeInUpBig'
+          style={styles.footer}>
+           <Text style={styles.text_footer}> Name</Text>
+           <View style={styles.action}
+              styles={{flexDirection:'row'}}>
+            
+             <Input
+              style={styles.text_input}
+              color="#14213d"
+              placeholder='Jason Derulo'
+              onChangeText={(val)=> nameInputChange(val)}
+              leftIcon={
+              <Icon
+              type= 'font-awesome'
+              name='user'
+              size={20}
+              color='#14213d'
+                /> 
+              }
+             /> 
+              {data.check_nameInputChange ?
+             <Animatable.View
+             animation='bounceIn'>
+               
+             <Feather
+               name='check-circle'
+               color='#14213d'
+               size={20}
+             />  
+             </Animatable.View>
+             
+             :null}
+           </View>
+           <Text style={styles.text_footer}> Email</Text>
+           <View style={styles.action}
+              styles={{flexDirection:'row'}}>
+            
+             <Input
+              style={styles.text_input}
+              autoCapitalize='none'
+              color="#14213d"
+              placeholder='jasonD@you.com'
+              onChangeText={(val)=> textInputChange(val)}
+              leftIcon={
+              <Icon
+              type= 'font-awesome'
+              name='user'
+              size={20}
+              color='#14213d'
+                /> 
+              }
+             /> 
+              {data.check_textInputChange ?
+             <Animatable.View
+             animation='bounceIn'>
+               
+             <Feather
+               name='check-circle'
+               color='#14213d'
+               size={20}
+             />  
+             </Animatable.View>
+             
+             :null}
+           </View>
+           <Text 
+           style={{marginTop:5, fontSize:18,color:'#14213d'}}> 
+           Password
+           </Text>
+           <View style={styles.action}
+              >
+             <Input
+              style={styles.text_input}
+              secureTextEntry={data.secureTextEntry? true :false}
+              autoCapitalize='none'
+              color="#14213d"
+              placeholder='Your Password'
+              onChangeText={(val)=> handlePasswordChange(val)}
+              leftIcon={
+              <Icon
+              type= 'font-awesome'
+              name='lock'
+              size={20}
+              color='#14213d'
+                /> 
+              }
+             /> 
+             <TouchableOpacity
+               onPress={updateSecureTextEntry}>
+              {data.secureTextEntry?   
+             <Feather
+               name='eye-off'
+               color='#6b705c'
+               size={20}
+             />
+             :
+             <Feather
+               name='eye'
+               color='#6b705c'
+               size={20}
+             />
+              }
+             </TouchableOpacity>
+           </View>
+           <Text 
+           style={{marginTop:5, fontSize:18,color:'#14213d'}}> 
+           Confirm Password
+           </Text>
+           <View style={styles.action}
+              >
+             <Input
+              style={styles.text_input}
+              secureTextEntry={data.confirm_secureTextEntry? true :false}
+              autoCapitalize='none'
+              color="#14213d"
+              placeholder='Your Password'
+              onChangeText={(val)=> handleConfirmPasswordChange(val)}
+              leftIcon={
+              <Icon
+              type= 'font-awesome'
+              name='lock'
+              size={20}
+              color='#14213d'
+                /> 
+              }
+             /> 
+             <TouchableOpacity
+               onPress={updateConfirmSecureTextEntry}>
+              {data.secureTextEntry?   
+             <Feather
+               name='eye-off'
+               color='#6b705c'
+               size={20}
+             />
+             :
+             <Feather
+               name='eye'
+               color='#6b705c'
+               size={20}
+             />
+              }
+             </TouchableOpacity>
+           </View>
+           <View style={styles.button}>
+             <LinearGradient
+              style={styles.signIn}
+              colors={['#2c7da0','#98c1d9']}
+              >
+              <Text
+               style={styles.textSign}>
+               Sign Up 
+              </Text>
+             </LinearGradient> 
+  {/*  
+             <TouchableOpacity
+              onPress={()=>navigation.push('Signup')}
+              style={[styles.signIn,
+              {
+              borderColor:'#fb8500',
+              borderWidth:1,
+              marginTop:15,
+              }]}>
+               <Text 
+               style={{
+               color:'#023047',
+               fontWeight:'bold'}}>
+                 SignUp
+                 </Text>
+             </TouchableOpacity> */}
+           </View>
+         </Animatable.View>
+       </View>
+    );
+};
+
+export default Signup;
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#98c1d9',
+    },
+    
+    button:{
+      alignItems:'flex-end',
+      marginTop:16,
+    },
+    signIn:{
+     width:'100%',
+     height:50,
+     justifyContent:'center',
+     alignItems:'center',
+     borderRadius:10,
+    },
+    textSign:{
+     color:'white',
+     fontWeight:'bold',
+    },
+    
+    footer:{
+      flex:3,
+      backgroundColor: '#fff',
+      borderTopRightRadius:30,
+      borderTopLeftRadius:30,
+      paddingVertical:15,
+      paddingHorizontal:30,
+      
+    },
+    header:{
+      flex:1,
+      justifyContent:'flex-end',
+      paddingHorizontal:20,
+      paddingBottom:15
+    },
+    text_header:{
+      color:'white',
+      fontWeight:'bold',
+      fontSize:30
+    },
+    text_footer:{
+      color:'#14213d',
+      fontSize:18
+    },
+    action:{
+      flexDirection:'row',
+      marginTop:5,
+      borderBottomWidth:1,
+      borderBottomColor:'#f2f2f2',
+      paddingBottom:2
+    },
+    text_input:{
+      flex:1,
+      marginTop:Platform.OS === 'ios' ? 0 : -10,
+      paddingLeft:10,
+      color:'#14213d'
+    }
+});
