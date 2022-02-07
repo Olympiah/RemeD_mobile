66a2899ae107e68c77b849892c9635d4b57d0090
Jayem-11 <ronojohnmichael@gmail.com>
2022-02-07 11:20:01 +0300

    routed sign up

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,10 +4,8 @@ const HomeScreen = ({navigation}) => {
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
       {/* <Text>Home Screen</Text> */}
-      <Button title='Doctor Profile' onPress={() => navigation.navigate('DoctorDash',{name: 'Doctor'})}/>
-      <Button color='black' title='Patient Profile' onPress={() => navigation.navigate('PatientDash',{name: 'Patient'})}/>
-      <Button color='black' title='Login' onPress={() => navigation.navigate('Login')}/>
-      <Button color='black' title='Register' onPress={() => navigation.navigate('Register')}/>
+      <Button title='Doctor Dashboard' onPress={() => navigation.navigate('DoctorDash',{name: 'Doctor'})}/>
+      <Button color='black' title='Patient Dashboard' onPress={() => navigation.navigate('PatientDash',{name: 'Patient'})}/>
       <Button color='dodgerblue' title='Faqs' onPress={() => navigation.navigate('Faqs',{name: 'Faqs'})}/>
       {/* <Button color='black' title='Landing' onPress={() => navigation.navigate('Landing')}/> */}
     </View>
