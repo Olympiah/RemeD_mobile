2312d62893c46bb9d6349f51470d68c58f8499f1
sonylomo <sonylomo1@gmail.com>
2022-02-23 11:38:57 +0300

    The great purge 🗡

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,28 @@
-import { View, Text, Button, TouchableOpacity } from 'react-native';
-import useAuth from '../hooks/useAuth';
+import { Button, View } from "react-native";
+import useAuth from "../hooks/useAuth";
 
 const HomeScreen = ({ navigation }) => {
-    const { logout } = useAuth();
-    return (
-        <View style={{ flex: 1, justifyContent: "center" }}>
-            {/* <Text>Home Screen</Text> */}
-            <Button title='Doctor Dashboard' onPress={() => navigation.navigate('DoctorDash', { name: 'Doctor' })} />
-            <Button color='black' title='Patient Dashboard' onPress={() => navigation.navigate('PatientDash', { name: 'Patient' })} />
-            <Button color='dodgerblue' title='Faqs' onPress={() => navigation.navigate('Faqs', { name: 'Faqs' })} />
-            <Button color='black' title='Logout' onPress={logout} />
-        </View>
-    );
+  const { logout } = useAuth();
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      {/* <Text>Home Screen</Text> */}
+      <Button
+        title="Doctor Dashboard"
+        onPress={() => navigation.navigate("DoctorDash", { name: "Doctor" })}
+      />
+      <Button
+        color="black"
+        title="Patient Dashboard"
+        onPress={() => navigation.navigate("PatientDash", { name: "Patient" })}
+      />
+      <Button
+        color="dodgerblue"
+        title="Faqs"
+        onPress={() => navigation.navigate("Faqs", { name: "Faqs" })}
+      />
+      <Button color="black" title="Logout" onPress={logout} />
+    </View>
+  );
 };
 
-
-export default HomeScreen; 
+export default HomeScreen;
